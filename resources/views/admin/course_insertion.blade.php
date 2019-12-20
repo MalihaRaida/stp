23153ffab4093813784f55d076a68dc73cab2fe9
Zannatun Naim Sristy <zannatunnaim@iut-dhaka.edu>
2019-12-20 15:59:52 +0600

    course_insertion

--- resources/views/admin/course_insertion.blade.php
+++ resources/views/admin/course_insertion.blade.php
@@ -23,7 +23,7 @@
                             <th width="15%">C Code</th>
                             <th width="30%">Course Title</th>
                             <th width="15%">Department</th>
-                            <th width=20%">Semester</th>
+                            <th width="20%">Semester</th>
                             <th width="10%">Credit</th>
                         </tr>
                     </thead>
@@ -81,10 +81,28 @@
                 <td><button class='btn btn-flat btn-danger remove' id='"+no+"'><i class='fa fa-close'></i></button></td>\n\
                 <td><input type='text' class='form-control' name='c_code[]' id='c_code"+no+"'></td>\n\
                 <td><input type='text' class='form-control' name='c_name[]' id='c_name"+no+"'></td>\n\
+                <td><select class="select2 form-control" id="dept" name="dept[]">\n\
+                                    <option>Please Select</option>\n\
+                                    <option value="CSE">CSE</option>\n\
+                                    <option value="EEE">EEE</option>\n\
+                                    <option value="MCE">MCE</option>\n\
+                                    <option value="CEE">CEE</option>\n\
+                                </select>\n\
+                             </td> \n\
+                             <td><select class="select2 form-control" id="sem" name="sem[]">\n\
+                                    <option>Please Select</option>\n\
+                                    <option value="1st Semester">1st Semester</option>\n\
+                                    <option value="2nd Semester">2nd Semester</option>\n\
+                                    <option value="3rd Semester">3rd Semester</option>\n\
+                                    <option value="4th Semester">4th Semester</option>\n\
+                                    <option value="5th Semester">5th Semester</option>\n\
+                                    <option value="6th Semester">6th Semester</option>\n\
+                                    <option value="7th Semester">7th Semester</option>\n\
+                                    <option value="8th Semester">8th Semester</option>\n\
+                         </select>\n\
+                             </td> \n\
                 <td><input type='text' class='form-control' name='credit[]' id='credit"+no+"'></td>\n\
             </tr>");
-        $('#sem'+no).val($('#per').val());
-        $('#prog'+no).val($('#program_code').val());
     }
     e.preventDefault();
     return false;
